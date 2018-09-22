16b19a72b178d361db8e405db3ae693312b94644
ark120202 <ark120202@gmail.com>
2018-09-22 23:01:27 +0500

    feat: add disable help button

--- game/scripts/vscripts/addon_game_mode.lua
+++ game/scripts/vscripts/addon_game_mode.lua
@@ -529,3 +529,16 @@ function SendMatchResults(winnerTeam)
 		end
 	end
 end
+
+CustomGameEventManager:RegisterListener("set_disable_help", function(_, data)
+	local to = data.to;
+	if PlayerResource:IsValidPlayerID(to) then
+		local playerId = data.PlayerID;
+		local disable = data.disable == 1
+		PlayerResource:SetUnitShareMaskForPlayer(playerId, to, 4, disable)
+
+		local disableHelp = CustomNetTables:GetTableValue("disable_help", tostring(playerId)) or {}
+		disableHelp[tostring(to)] = disable
+		CustomNetTables:SetTableValue("disable_help", tostring(playerId), disableHelp)
+	end
+end)
