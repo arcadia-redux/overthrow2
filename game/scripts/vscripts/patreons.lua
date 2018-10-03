5d1652360531a5a69199709b82bb43b59d8977a7
darklordabc <curtisjdubois@hotmail.com>
2018-10-03 23:32:09 +0100

    Another Patron

--- game/scripts/vscripts/patreons.lua
+++ game/scripts/vscripts/patreons.lua
@@ -2,6 +2,7 @@ local patreonLevels =  {
 	--["76561198054179075"] = 1, -- Darklord
 	["76561198047011112"] = 1, -- https://www.patreon.com/user?u=13950990
 	["76561197969637836"] = 1, -- https://www.patreon.com/user?u=13950995
+	["76561198055263275"] = 1, -- https://www.patreon.com/user?u=4469824
 }
 
 function GetPlayerPatreonLevel(playerId)
