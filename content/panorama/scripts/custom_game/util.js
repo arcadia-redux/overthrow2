e01d6f2df6e89c6e4facfe47906b345e1a8be2b9
EarthSalamander #42 <jeizyy30@hotmail.fr>
2018-11-29 15:38:26 +0100

    * Load Scren is now visible

--- content/panorama/scripts/custom_game/util.js
+++ content/panorama/scripts/custom_game/util.js
@@ -15,3 +15,31 @@ function SubscribeToNetTableKey(tableName, key, callback) {
 		if (currentKey === key) callback(value || {});
   });
 }
+
+function GetDotaHud() {
+	var p = $.GetContextPanel();
+	while (p !== null && p.id !== 'Hud') {
+		p = p.GetParent();
+	}
+	if (p === null) {
+		throw new HudNotFoundException('Could not find Hud root as parent of panel with id: ' + $.GetContextPanel().id);
+	} else {
+		return p;
+	}
+}
+
+function FindDotaHudElement(id) {
+	return GetDotaHud().FindChildTraverse(id);
+}
+
+function FillTopBarPlayer(TeamContainer) {
+	// Fill players top bar in case on partial lobbies
+	var playerCount = TeamContainer.GetChildCount();
+	for (var i = playerCount + 1; i <= 12; i++) {
+		var newPlayer = $.CreatePanel('DOTATopBarPlayer', TeamContainer, 'RadiantPlayer-1');
+		if (newPlayer) {
+			newPlayer.FindChildTraverse('PlayerColor').style.backgroundColor = '#FFFFFFFF';
+		}
+		newPlayer.SetHasClass('EnemyTeam', true);
+	}
+}
