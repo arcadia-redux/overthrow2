94abdb29fdf372e563d5895c46e905a044bbc877
Niker323 <artiom123.budko@yandex.by>
2019-02-24 07:38:00 +0300

    Revert "Fix hotfix"
    This reverts commit 616b5d3db34e6ad99a17142c5ec2683a519e9ca9.

--- game/scripts/vscripts/addon_game_mode.lua
+++ game/scripts/vscripts/addon_game_mode.lua
@@ -943,8 +943,8 @@ function COverthrowGameMode:OnItemPurchased( keys )
 			local item = hero:GetItemInSlot(i)
 			if item ~= nil then
 				if item:GetName() == itemname then
-					hero:RemoveItem(item)
 					hero:AddItemByName(itemname)
+					hero:RemoveItem(item)
 					--print(i)
 					return
 				end
