18288b9d7bbb515fdeab82f8249585d0b0dbb480
ark120202 <ark120202@gmail.com>
2019-09-08 11:41:17 +0500

    chore(panorama): remove unused `SendWebApiRequest` utility function

--- content/panorama/scripts/custom_game/util.js
+++ content/panorama/scripts/custom_game/util.js
@@ -1,13 +1,3 @@
-var WEB_API_TESTING = Game.IsInToolsMode() && false;
-var serverHost = WEB_API_TESTING ? 'http://127.0.0.1:5000' : 'http://163.172.174.77:8000';
-
-function SendWebApiRequest(path, data, onSuccess, onError) {
-    var settings = {type: 'POST', data: data};
-    if (onSuccess != null) settings.success = onSuccess;
-    if (onError != null) settings.error = onError;
-    $.AsyncWebRequest(serverHost + '/api/vscripts/' + path, settings);
-}
-
 function SubscribeToNetTableKey(tableName, key, callback) {
     var immediateValue = CustomNetTables.GetTableValue(tableName, key) || {};
     if (immediateValue != null) callback(immediateValue);
