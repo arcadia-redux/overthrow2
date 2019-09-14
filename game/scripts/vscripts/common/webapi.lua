538159121e44a487ba8a045a61474e6bba8cf164
ark120202 <ark120202@gmail.com>
2019-09-14 19:11:58 +0500

    feat(webapi): log debug messages only when `isTesting` is enabled

--- game/scripts/vscripts/common/webapi.lua
+++ game/scripts/vscripts/common/webapi.lua
@@ -1,7 +1,7 @@
 WebApi = WebApi or {}
 
-local isTesting = IsInToolsMode()
-local serverHost = isTesting and "http://127.0.0.1:5000" or "http://163.172.174.77:8000"
+local isTesting = IsInToolsMode() and false
+local serverHost = IsInToolsMode() and "http://127.0.0.1:5000" or "http://163.172.174.77:8000"
 local dedicatedServerKey = GetDedicatedServerKeyV2("1")
 
 function WebApi:Send(path, data, onSuccess, onError)
